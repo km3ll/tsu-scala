29b6df078c4f9abbaa5b157c86efddf1e7a8c160
km3ll <kmell@protonmail.com>
2023-02-23 00:45:38 -0500

    Format code and documentation

--- src/test/scala/pod/typelevel/typeclass/state/StateTests.scala
+++ src/test/scala/pod/typelevel/typeclass/state/StateTests.scala
@@ -13,22 +13,16 @@ class StateTests extends AnyFunSuite {
   }
 
   test( "Build robot using State.run" ) {
-    // Given
     val seed = Seed( 32L )
-    // When
     val ( state, robot ) = buildRobot.run( seed ).value
-    // Then
     println( s"Initial state: $seed, final state: $state, robot: $robot" )
     assert( robot.id == seed.long )
   }
 
   test( "Build robot using State.runA" ) {
-    // Given
     val seed = Seed( 32L )
-    // When
     // runA: Run with the provided initial state value and return the final value (discarding the final state).
     val robot = buildRobot.runA( seed ).value
-    // Then
     println( s"Initial state: $seed, robot: $robot" )
     assert( robot.id == seed.long )
   }
