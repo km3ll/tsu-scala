500a5ed7532d60c85306c8503a6a154ad736e0e7
km3ll <kmell@protonmail.com>
2025-01-28 00:48:10 -0500

    Pod 153: Synchronize pod data

--- learn-basics/src/main/scala/pod/tsu/scala/basics/logging/LoggerApp.scala
+++
@@ -1,9 +0,0 @@
-package pod.tsu.scala.basics.logging
-
-import com.typesafe.scalalogging.LazyLogging
-
-object LoggerApp extends LazyLogging {
-  def main( args: Array[String] ): Unit = {
-    logger.info( "Hello Pod" )
-  }
-}
